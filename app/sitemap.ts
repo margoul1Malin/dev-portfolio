866c7b8d879fb4a6e823a601f559b44fe3ee0e07
margoul1Malin <github.pydoctor@gmail.com>
2025-07-29 21:21:40 +0200

    sdsdkqjshfkjzqhrkjhazqkjazerkazrazlkhevhjfgazkjqrjkazhihzaesdsd

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,30 +12,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/#about`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#skills`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#projects`,
-      lastModified: currentDate,
-      changeFrequency: 'weekly',
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#contact`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
-    {
       url: `${baseUrl}/contact`,
       lastModified: currentDate,
       changeFrequency: 'monthly',
